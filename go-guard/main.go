acdca308198c0909298289aeb4ca53ece08d8cf6
Farbod Salimi <farbod_salimi@yahoo.com>
2019-12-22 00:54:40 -0800

    Update go-guard/main.go

---
+++ go-guard/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strconv"
+)
+
+type transport struct {
+	http.RoundTripper
+}
+
+func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+	resp, err = t.RoundTripper.RoundTrip(req)
+	if err != nil {
+		return nil, err
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	b = bytes.Replace(b, []byte("server"), []byte("schmerver"), -1)
+	body := ioutil.NopCloser(bytes.NewReader(b))
+
+	log.Println(body)
+
+	resp.Body = body
+	resp.ContentLength = int64(len(b))
+	resp.Header.Set("Content-Length", strconv.Itoa(len(b)))
+	resp.Header.Add("server", "go-guard")
+	log.Println(resp.Header)
+
+	return resp, nil
+}
+
+var _ http.RoundTripper = &transport{}
+
+func main() {
+	target, err := url.Parse("http://localhost:3000")
+	if err != nil {
+		panic(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	proxy.Transport = &transport{http.DefaultTransport}
+
+	http.ListenAndServe(":8080", proxy)
+}
